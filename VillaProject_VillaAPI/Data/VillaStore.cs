142c4cc62fcb1eafd0599410fdd0fe44454a3c37
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-08-02 17:16:42 +0300

    api crud tamamlandi

---
+++ VillaProject_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using VillaProject_VillaAPI.Models.Dto;
+
+namespace VillaProject_VillaAPI.Data
+{
+    public class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+         {
+                new VillaDTO {Id=1,Name="Havuz Manzaralı", Occupancy=4 ,Sqft=100},
+                new VillaDTO {Id=2,Name="Deniz Manzaralı",Occupancy=3 ,Sqft=400}
+         };
+    }
+}
---
+++ VillaProject_VillaAPI/Models/Villa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VillaProject_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
